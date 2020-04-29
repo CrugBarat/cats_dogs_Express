88b721338174bdabff7c962f986301ee96033abc
Craig Barratt <craigtbarratt@gmail.com>
2020-04-29 20:53:42 +0100

    adds basic setup

--- server/server.js
+++ server/server.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
+app.use(cors());
+app.use(bodyParser.json());
+
+app.listen(3000, function() {
+  console.log(`App is running on port ${this.address().port}`);
+});

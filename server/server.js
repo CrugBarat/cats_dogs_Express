4bdad7a077f71be1ddc466211a4b82643ab223c7
Craig Barratt <craigtbarratt@gmail.com>
2020-04-29 20:56:22 +0100

    adds cats and dogs data

--- server/server.js
+++ server/server.js
@@ -3,6 +3,22 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
+const dogs = [
+              {name: "Fluffy", breed: "Pomeranian"},
+              {name: "Kane", breed: "Rottweiler"},
+              {name: "Bonzo", breed: "German Shepherd"},
+              {name: "Lucky", breed: "Cockapoo"},
+              {name: "Luna", breed: "Husky"}
+            ];
+
+const cats = [
+              { name: "Tiddles", breed: "Siberian" },
+              { name: "Patch", breed: "Bengal" },
+              { name: "Socks", breed: "British Shorthair" },
+              { name: "Mr Bigglesworth", breed: "Persian" },
+              { name: "Monty", breed: "Maine Coon" }
+            ];            
+
 app.use(cors());
 app.use(bodyParser.json());
 
